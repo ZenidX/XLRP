fa370f59e4ac3849dcb1efb333841eb8d42ff826
Luis Martinez <luismartinez.rodriguez.1@gmail.com>
2022-09-14 14:03:38 +0200

    Hago los resultados y cosas de JS

--- XLRP/src/main/resources/static/buscador/main.js
+++ XLRP/src/main/resources/static/buscador/main.js
@@ -1,5 +1,14 @@
-var search=document.getElementById("search");
 var resultados=document.getElementById("resultados");
-search.oninput= function buscar() {
-    resultados.classList.toggle(display)
+function buscar() {
+    var search=document.getElementById("search").value;
+    resultados.innerHTML=search;
+
+}
+function cambiar(){
+    var select = document.getElementById('seleccionar');
+    var valor = select.selectedIndex;
+    resultados.innerHTML=select.options[valor].text;
+    if (select.options[valor].value=="Actividades"){
+        document.getElementById("search").value=select.options[valor].text
+    }
 }
