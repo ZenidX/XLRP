3c15f67bc4bf2de08c3773f9a98b6024d3c2b7e9
Ruben Medina Martinez <ruskymartinez@gmail.com>
2022-09-20 11:02:50 +0200

    Actualizacion

--- XLRP/src/main/java/xlrp/service/ClientesService.java
+++ XLRP/src/main/java/xlrp/service/ClientesService.java
@@ -1,5 +1,12 @@
 package xlrp.service;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import xlrp.entities.Cliente;
+import xlrp.dao.ClienteDAO;
+
 public class ClientesService {
 
 }
