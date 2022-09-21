bca5170a144962caa93c6ea9e6f58c26c0c49fa4
ZenidX <zenid77@gmail.com>
2022-09-21 13:34:30 +0200

    intento de registro

--- BACK/XLRP/src/main/java/xlrp/service/PerfilesService.java
+++ BACK/XLRP/src/main/java/xlrp/service/PerfilesService.java
@@ -4,12 +4,16 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import org.slf4j.LoggerFactory;
+import ch.qos.logback.classic.Logger;
+import xlrp.XLRPApplication;
 import xlrp.dao.PerfilDAO;
 import xlrp.entities.Perfil;
 
 
 @Service
 public class PerfilesService {
+	private static final Logger LOGGER=(Logger) LoggerFactory.getLogger(XLRPApplication.class);
 	@Autowired
 	PerfilDAO perfilDAO;
 	public boolean autentificacion(Perfil perfil_sesion){
@@ -34,6 +38,10 @@ public class PerfilesService {
 		return perfilDAO.count();
 	}
 	public Perfil registro(Perfil perfil_registro){
+
+		LOGGER.info(perfil_registro.toString());
+		return perfil_registro;
+		/*
 		perfilDAO.saveAndFlush(perfil_registro);
 		List<Perfil> perfiles=perfilDAO.findAll();
 		int i=0;
@@ -46,5 +54,6 @@ public class PerfilesService {
 			i++;
 		}
 		return perfiles.get(i);
+		*/
 	}
 }
