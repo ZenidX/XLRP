b78dffb0451c17d3f999713b100db595fe0cb7f7
ZenidX <zenid77@gmail.com>
2022-09-23 16:03:22 +0200

    Fronteando con fetch

--- FRONT/paginaweb/main.js
+++ FRONT/paginaweb/main.js
@@ -102,16 +102,23 @@ formularioRegistro.onsubmit = async (e) => {
 };
 
 /* Llamar a servicios desde el buscador*/
-async function fetchJSON(url) {
-    const response = await fetch(url);
+async function fetchJSON(url1) {
+    const response = await fetch(url1);
     const jsonResponse = await response.json();
     return jsonResponse;
 } 
-const url = 'http://localhost:8080/api/servicios'
-fetchJSON(url).then(json => {
-    document.getElementById("serdes").innerHTML = json[0].descripcion;
-    console.log("Adios");
-});
+var id_servicio=1;
+var url_servicios = 'http://localhost:8080/api/servicios/';
+var url_profesionales='http://localhost:8080/api/perfiles/';
+fetchJSON(url_servicios+id_servicio).then(json => {
+    document.getElementById("serdes").innerHTML = json.descripcion;
+    console.log("Hemos cambiado la descripcion con el json del servicio");
+    return json.id_profesional;
+}).then(id_profesional=>{
+    prof_json=fetchJSON(url_profesionales+id_profesional);
+    document.getElementById("nombre_profesional").innerHTML = json_prof.nombre+' '+json_prof.apellidos;
+    console.log("Hemos cambiado el nombre y apellidos con el json del profesional que ponia en el json del servicio");
+})
 
 /* Llamar API y json*/
 /*async function fetchJSON(url) {
