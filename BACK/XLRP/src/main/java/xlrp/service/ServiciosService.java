a5ea88691a57925bb0503446cf0c502cbee1d740
ZenidX <zenid77@gmail.com>
2022-09-23 12:27:16 +0200

    arreglando detalitos de packages back

--- BACK/XLRP/src/main/java/xlrp/service/ServiciosService.java
+++ BACK/XLRP/src/main/java/xlrp/service/ServiciosService.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import xlrp.dao.ServicioDAO;
-import xlrp.entities.Perfil;
 import xlrp.entities.Servicio;
 
 @Service
