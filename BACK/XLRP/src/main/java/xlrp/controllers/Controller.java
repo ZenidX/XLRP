c6fd64f35529548831fa3a3b5748cd5b7c352438
Ruben Medina Martinez <ruskymartinez@gmail.com>
2022-09-25 20:43:15 +0200

    end points probados

--- BACK/XLRP/src/main/java/xlrp/controllers/Controller.java
+++ BACK/XLRP/src/main/java/xlrp/controllers/Controller.java
@@ -44,22 +44,31 @@ public class Controller {
 	public ResponseEntity<Optional<Perfil>> getPerfil(@PathVariable long id){
 		return ResponseEntity.ok(perfilesService.perfilPorId(id));
 	}
+	
+	//el perfiles/delete/{id} (error 500).------------------------------------------------
 	@RequestMapping(value="/perfiles/delete/{id}", method=RequestMethod.DELETE)
 	public void deletePerfil(@PathVariable Long id){
 		perfilDAO.deleteById(id);
 	}
-	@RequestMapping(value="/perfil/registrar",method=RequestMethod.POST)
+	
+	//el perfiles/registrar creo que no soy yo el que lo tiene que probar (error 400).-------------
+	@RequestMapping(value="/perfiles/registrar",method=RequestMethod.POST)
 	public ResponseEntity<Perfil> registrarse1(@RequestBody Perfil perfil_registro){
 		return ResponseEntity.ok(perfilesService.registrarPerfil(perfil_registro));
 	}
+	
+	//no puedo editar con el perfiles/editar (error 400)-------------------------------------------
 	@RequestMapping(value="/perfiles/editar",method=RequestMethod.POST)
 	public ResponseEntity<Perfil> editarPerfil(@RequestBody Perfil perfil_editado){
 		return ResponseEntity.ok(perfilesService.editarPerfil(perfil_editado));
 	}
+	
+	//Servicios (error 405).--------------------------------------------------------------
 	@RequestMapping(value="/servicios",method=RequestMethod.GET)
 	public ResponseEntity<List<Servicio>> getAllServicios(){
 		return ResponseEntity.ok(serviciosService.allServicios());
 	}
+	
 	@RequestMapping(value="/servicios/{id_servicio}", method=RequestMethod.GET)
 	public ResponseEntity<Optional<Servicio>> PeticionServicios(@PathVariable long id_servicio){
 		return ResponseEntity.ok(serviciosService.servicioPorId(id_servicio));
@@ -68,30 +77,43 @@ public class Controller {
 	public ResponseEntity<List<Servicio>> servicioPorId_profesional(@PathVariable long id_profesional){
 		return ResponseEntity.ok(serviciosService.serviciosPorId_profesional(id_profesional));
 	}
-	@RequestMapping(value="/servicio/delete/{id_servicio}",method=RequestMethod.DELETE)
+	
+	//servicios/delete/{id_servicios} no funciona (error 404).----------------------------
+	@RequestMapping(value="/servicios/delete/{id_servicio}",method=RequestMethod.DELETE)
 	public void eliminarServicio(@PathVariable long id_servicio){
 		servicioDAO.deleteById(id_servicio);
 	}
+	
+	//no se como registrar yo desde postman---------------------------------------------
 	@RequestMapping(value="/servicios/registrar",method=RequestMethod.POST)
 	public ResponseEntity<Servicio> registrarServicio(@RequestBody Servicio servicio_registrado){
 		return ResponseEntity.ok(serviciosService.registrarServicio(servicio_registrado));
 	}
+	
+	//Este no se como comprobar la edicion (error 400)-------------------------------------
 	@RequestMapping(value="/servicios/editar",method=RequestMethod.POST)
 	public ResponseEntity<Servicio> editarServicio(@RequestBody Servicio servicio_editado){
 		return ResponseEntity.ok(serviciosService.editarServicio(servicio_editado));
 	}
+	
 	@RequestMapping(value="/clientes",method=RequestMethod.GET)
 	public ResponseEntity<List<Cliente>> getClientes(){
 		return ResponseEntity.ok(clientesService.allClientes());
 	}
+	
+	//Este (error 405)----------------------------------------------------------------------
 	@RequestMapping(value="/clientes/{id_cita}",method=RequestMethod.POST)
 	public ResponseEntity<Optional<Cliente>> clientePorId(@PathVariable long id_cita){
 		return ResponseEntity.ok(clientesService.clientePorId_cita(id_cita));
 	}
-	@RequestMapping(value="/clientes/servicio/{id_servicio}",method=RequestMethod.GET)
+	
+	//Este (error 404)-----------------------------------------------------------------------
+	@RequestMapping(value="/clientes/servicios/{id_servicio}",method=RequestMethod.GET)
 	public ResponseEntity<List<Cliente>> clientesPorId_servicio(@PathVariable long id_servicio){
 		return ResponseEntity.ok(clientesService.clientesPorId_servicio(id_servicio));
 	}
+	
+	
 	@RequestMapping(value="/clientes/profesional/{id_profesional}",method=RequestMethod.GET)
 	public ResponseEntity<List<Cliente>> clientesPorId_profesional(@PathVariable long id_profesional){
 		return ResponseEntity.ok(clientesService.clientesPorId_profesional(id_profesional));
